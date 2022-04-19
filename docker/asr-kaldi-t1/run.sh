c160d06f45f3387013d88d5a347ecf381a1b3b51
mostafaashahin <mostafa_shahin@ieee.org>
2022-04-19 22:20:10 +1000

    adding docker for ibm kaldi asr task1

---
+++ docker/asr-kaldi-t1/run.sh
@@ -0,0 +1,9 @@
+cat - | while read id
+do
+	if [ ! -f /opt/data/$id/txtgrids/${id}_task1_ibm.TextGrid ]; then
+	    python3 steps/Run_IBM_ASR.py /opt/data/$id/txtgrids/ $id task1
+	fi
+	if [ ! -f /opt/data/$id/txtgrids/${id}_task1_kaldi.TextGrid ]; then
+	    python3 steps/Run_Kaldi_on_IBM.py /opt/data/$id/txtgrids/ $id task1
+	fi
+done
